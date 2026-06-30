c442123b3d2ae3d2eaa77585b2368ed4f0f4b644
dmTung <tungxixoan@gmail.com>
2026-06-30 23:43:02 +0700

    feat(plan2): add Hive + uuid deps, async Hive init in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox<String>('vocab_records');
+  await Hive.openBox<String>('topics');
   runApp(const ProviderScope(child: LexiCoreApp()));
 }
 
