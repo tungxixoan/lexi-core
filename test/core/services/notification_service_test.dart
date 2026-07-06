5ceb3cc55f22a947f434995427e649f1b51dd585
dmTung <tungxixoan@gmail.com>
2026-07-06 23:43:50 +0700

    fix(plan6): mock notification plugin channel in tests

--- test/core/services/notification_service_test.dart
+++ test/core/services/notification_service_test.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lexi_core/core/services/notification_service.dart';
 
 void main() {
   setUpAll(() {
     TestWidgetsFlutterBinding.ensureInitialized();
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      const MethodChannel('dexterous.com/flutter/local_notifications'),
+      (MethodCall methodCall) async => true,
+    );
   });
 
   test('NotificationService can be constructed without error', () {
